81097f88c886022462c7f416ecd260f025a12461
chauabhi <abhinavuiit@gmail.com>
2021-07-28 22:24:17 +0530

    07/28 group

---
+++ UserManagement/src/main/java/com/siemens/logistics/user_management/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.siemens.logistics.user_management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer implements WebApplicationInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserManagementApplication.class);
+    }
+}
